03ba8b0b8707f0645f5000ad2847ef4aed9eaca0
Dov Murik <dov.murik1@il.ibm.com>
2020-08-27 06:33:36 +0000

    read-ovmf-log: Better handling of ascii strings

--- read-ovmf-log.py
+++ read-ovmf-log.py
@@ -3,20 +3,25 @@ import re
 import struct
 import sys
 
+def is_all_printable(buf):
+    for b in buf:
+        if b < 32 or b > 127:
+            return False
+    return True
+
 def repl_hex(m):
     the_bytes = m[1]
-    hex_val = 'HEXDECODED=0x{:016x}'.format(struct.unpack('q', the_bytes)[0])
-    if the_bytes.isalnum():
-        hex_val += ' ASCII=' + the_bytes.decode('ascii')
+    hex_val = 'HEXDECODED=0x{:016x}'.format(struct.unpack('Q', the_bytes)[0])
+    if is_all_printable(the_bytes):
+        hex_val += ' ASCII=|{}|'.format(the_bytes.decode('ascii'))
     return hex_val.encode('utf-8')
 
 def process(filename):
     try:
         with open(filename, 'rb') as f:
             buf = f.read()
-            new_buf = re.sub(b'HEX\|(........)\|', repl_hex, buf)
+            new_buf = re.sub(b'HEX\|(........)\|', repl_hex, buf, flags=re.DOTALL)
             sys.stdout.buffer.write(new_buf)
-            #print(new_buf.decode("utf-8"))
     except BrokenPipeError:
         pass
 
